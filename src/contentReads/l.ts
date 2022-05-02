79cce66b14e05cc81d71e39a55e6a9ca1277484b
Syed Basim <social.syedbasim@gmail.com>
2022-05-02 05:43:21 +0300

    add w case

--- src/contentReads/l.ts
+++ src/contentReads/l.ts
@@ -3,5 +3,7 @@ import { Message } from 'discord.js';
 export const lHandler = (msg: Message) => {
 	if (msg.content.toLowerCase() === 'l') {
 		msg.channel.send('L');
+	} else if (msg.content.toLowerCase() === 'w') {
+		msg.channel.send('W');
 	}
 };
